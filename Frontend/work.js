33a9cc08133d83edc7aed0612349383cb809d219
Thibault Fregier <Tyshia.pro@gmail.com>
2024-05-30 16:20:18 +0200

    Fonction trier done

--- Frontend/work.js
+++ Frontend/work.js
@@ -1,9 +1,9 @@
 // Recuperation des information du serveur pour la gallery
 
-const reponse = await fetch(`http://localhost:5678/api/works`)
-const works = await reponse.json()
+const works = await fetch(`http://localhost:5678/api/works`).then(works => works.json())
 
-function genererworks(works){
+
+function generateWorks(works){
     for (let i = 0; i < works.length; i++){
         const workElement = works[i];
         // recuperation de l'élément du DOM qui acceuillera les figures
@@ -12,15 +12,57 @@ function genererworks(works){
         const figure = document.createElement("figure");
         // création de chaque balises
         const imageElement = document.createElement("img");
-        imageElement.src = workElement.imgageUrl;
+        imageElement.src = workElement.imageUrl;
         const titreElement = document.createElement("figcaption");
-        titreElement.innertext = workElement.title;
+        titreElement.innertext = workElement.title ?? "aucun titre";
 
     // rattachement des balises au DOM
-    gallery.appendChild(figure)
-    figure.appendChild(imageElement)
-    figure.appendChild(titreElement)
+    gallery.appendChild(figure);
+    figure.appendChild(imageElement);
+    figure.appendChild(titreElement);
     }
 }
 
-genererworks(works)
+generateWorks(works)
+
+// filtre tout afficher
+const buttonFilterAll = document.getElementById("filterAll");
+buttonFilterAll.addEventListener(`click`, function(){
+    const filterAll = works;
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(filterAll);
+});
+
+// filtre afficher category objet
+const buttonFilterItems = document.getElementById("FilterItems");
+buttonFilterItems.addEventListener(`click`, function(){
+    const Filtercategory = works.filter(function (work) {
+        return work.categoryId === 2;
+    })
+    
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(Filtercategory)
+})
+
+//filtre afficher category apartment
+
+const buttonFilterApartment = document.getElementById("filterApartment");
+buttonFilterApartment.addEventListener(`click`, function(){
+    const Filtercategory = works.filter(function (work) {
+        return work.categoryId === 1;
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(Filtercategory)
+})
+
+//filtre afficher category HotelsAndRestaurants
+
+const buttonFilterHotelsAndRestaurants = document.getElementById("filterHotelsAndRestaurants");
+buttonFilterHotelsAndRestaurants.addEventListener(`click`, function(){
+    const Filtercategory = works.filter(function (work) {
+        return work.categoryId === 3;
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(Filtercategory)
+})
+
